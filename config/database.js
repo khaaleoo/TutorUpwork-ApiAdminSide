f14807834b467cd551bbfc9bbe664de1b7ac9c49
wedday2004 <48178473+wedday2004@users.noreply.github.com>
2019-12-04 21:11:34 +0700

    fix

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 //Set up mongoose connection
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://lexuankha2409:123456kha@cluster0-bezjf.mongodb.net/TutorUpworkDB?retryWrites=true&w=majority';
+const mongoose = require("mongoose");
+const mongoDB =
+  "mongodb+srv://lexuankha2409:123456kha@cluster0-bezjf.mongodb.net/TutorUpworkDB?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
--- controllers/admins.js
+++ controllers/admins.js
@@ -1,21 +1,35 @@
-const adminModel = require('../models/admins');
+const adminModel = require("../models/admins");
 
 module.exports = {
-    create: function(req, res, next) {
-      adminModel.findOne({email: req.body.email}, function(err, doc){
-         if(err) throw err;
-         if (doc == null){
-            adminModel.create({ name: req.body.name, email: req.body.email, password: req.body.password, role: req.body.role }, function (err) {
-               if (err) 
-                next(err);
-               else
-                res.json({status: "success", message: "User added successfully!!!", data: null});
-               
-             });
-         }
-         else {
-            res.json({status: "failed", message: "Failed to create new User, Email has been registed before.", data: null})
-         }
-      })
-    }
-}
+  create: function(req, res, next) {
+    adminModel.findOne({ email: req.body.email }, function(err, doc) {
+      console.log(doc);
+      if (err) throw err;
+      if (doc == null) {
+        adminModel.create(
+          {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            role: req.body.role
+          },
+          function(err) {
+            if (err) next(err);
+            else
+              res.json({
+                status: "success",
+                message: "User added successfully!!!",
+                data: null
+              });
+          }
+        );
+      } else {
+        res.json({
+          status: "failed",
+          message: "Failed to create new User, Email has been registed before.",
+          data: null
+        });
+      }
+    });
+  }
+};
