8ad61c838381a6a495f807db0946dc995e9e8732
Kha Leo <lexuankha2409@gmail.com>
2019-12-04 19:34:13 +0700

    api add new Adminn

---
+++ controllers/admins.js
@@ -0,0 +1,21 @@
+const adminModel = require('../models/admins');
+
+module.exports = {
+    create: function(req, res, next) {
+      adminModel.findOne({email: req.body.email}, function(err, doc){
+         if(err) throw err;
+         if (doc == null){
+            adminModel.create({ name: req.body.name, email: req.body.email, password: req.body.password, role: req.body.role }, function (err) {
+               if (err) 
+                next(err);
+               else
+                res.json({status: "success", message: "User added successfully!!!", data: null});
+               
+             });
+         }
+         else {
+            res.json({status: "failed", message: "Failed to create new User, Email has been registed before.", data: null})
+         }
+      })
+    }
+}
---
+++ models/admins.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+var adminSchema = mongoose.Schema({
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [6, 'password must has more than 6 characters.']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    role: {
+        type: String,
+        trim: true, 
+        required: true
+    },
+});
+
+// hash user password before saving into database
+adminSchema.pre('save', function(next){
+    this.password = bcrypt.hashSync(this.password, saltRounds);
+    next();
+});
+
+// // check password valid?
+// userSchema.methods.validPassword = function (password) {
+//     return bcrypt.compareSync(password, this.password);
+// };
+
+const User = mongoose.model('admin',adminSchema);
+
+module.exports = User;
