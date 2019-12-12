7bd5eeba61262792702089e6bde46542de031663
Kha Leo <lexuankha2409@gmail.com>
2019-12-12 10:22:23 +0700

    fix bug homepage path / tt

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res) {
-  console.log("Listening on port 8000");
+router.get("/", function (req, res) {
   res.send("ApiClientSide");
 });
 
